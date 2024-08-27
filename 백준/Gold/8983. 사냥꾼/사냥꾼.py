8478444398a2bed59db6d28878baab010ddccf63
SeoHee Park <86008429+seohee-P@users.noreply.github.com>
2024-08-27 16:54:43 +0900

    [Gold IV] Title: 사냥꾼, Time: 276 ms, Memory: 50744 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,16 +1,37 @@
-M, N, L = map(int, input().split())
-mList = list(map(int, input().split()))
-nList = [list(map(int, input().split())) for _ in range(N)]
-caught = [0 for _ in range(N)]
+# 복붙 코드
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+
+# target과 가장 가까운 숫자를 리턴
+def findClosestNum(array, target):
+    index = bisect_left(array, target)
+    if index == 0:
+        return array[index]
+    
+    if index == len(array):
+        return array[-1]
+
+    left = array[index-1]
+    right = array[index]
+
+    if target-left < right - target:
+        return left
+    return right
+
+
+m, n, l = map(int, input().split())
+hunting_pos = sorted(list(map(int, input().split())))
+animals = [tuple(map(int, input().split())) for _ in range(n)]
 
 answer = 0
-for m in mList:
-    for i in range(N):
-        if caught[i] != 0:
-            continue
-        if abs(nList[i][0]-m) + nList[i][1] <= L:
-            answer += 1
-            caught[i] = 1
+# 각 동물이 사냥될 수 있는 사대의 위치를 모아본다.
+for x, y in animals:
+    # 현재 동물의 위치에서 가장 가까운 사대 찾기
+    closestHuntingPos = findClosestNum(hunting_pos, x)
 
-
+    # 사대까지의 거리가 사정거리 안에 있는지 확인해본다.
+    if x - (l-y) <= closestHuntingPos <= x + (l-y):
+        answer += 1
+        
 print(answer)
