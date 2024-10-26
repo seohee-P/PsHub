892100e9f2a6ba6e16ebd76fffc795796ce26c6e
SeoHee Park <86008429+seohee-P@users.noreply.github.com>
2024-10-26 23:48:28 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,8 +1,2 @@
-# 팰린드롬인지 확인하기
-def main():
-    s = input()
-    print(1) if s == s[::-1] else print(0)
-
-
-if __name__ == '__main__':
-    main()
+s = list(input())
+print(1) if list(reversed(s)) == s else print(0)
