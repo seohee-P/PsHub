c6b4767eef8148e70ad59df875485f654cf8e6a0
SeoHee Park <86008429+seohee-P@users.noreply.github.com>
2025-01-31 03:00:48 +0900

    [Gold V] Title: 탑, Time: 488 ms, Memory: 133644 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,18 +1,17 @@
-import heapq
-
+# 탑
 def main():
     N = int(input())
     A = list(map(int, input().split()))
-    heap = [(A[0], 0)]
+    stack = [(A[0], 0)]
     answer = [0 for _ in range(N)]
     for i in range(1, N):
-        while heap and heap[0][0] < A[i]:
-            heapq.heappop(heap)
-        if heap:
-            answer[i] = heap[0][1] + 1
+        while stack and stack[-1][0] < A[i]:
+            stack.pop()
+        if stack:
+            answer[i] = stack[-1][1] + 1
         else:
             answer[i] = 0
-        heapq.heappush(heap, (A[i], i))
+        stack.append((A[i], i))
     print(*answer)
 
 if __name__=='__main__':
