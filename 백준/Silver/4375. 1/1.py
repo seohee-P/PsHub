1250b61cfe8c02722a0f80b7acb29fb917e9e130
SeoHee Park <86008429+seohee-P@users.noreply.github.com>
2025-02-02 01:19:33 +0900

    [Silver III] Title: 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.py
+++ 백준/Silver/4375. 1/1.py
@@ -1,15 +1,14 @@
 # 1
+import sys
+
 def main():
-    while True:
-        try:
-            n = int(input())
-        except:
-            break
+    for line in sys.stdin:
+        n = int(line)
         now = 1
         answer = 1
         while now % n != 0:
             answer += 1
-            now = now * 10 + 1
+            now = (10 * now + 1)%n
         print(answer)
 
 if __name__ == '__main__':
