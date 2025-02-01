31ab119ae01003b65150b0bfd80376ff3a28be3a
SeoHee Park <86008429+seohee-P@users.noreply.github.com>
2025-02-01 19:14:17 +0900

    [Silver IV] Title: 주몽, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.py
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -2,20 +2,21 @@
 def main():
     N = int(input())
     M = int(input())
-    ingredients = sorted(list(map(int, input().split())))
+    ingredients = list(map(int, input().split()))
+    ingredients.sort()
     answer = 0
-    for i in range(N):
-        if M - ingredients[i] <= 0 or ingredients[i] == 0:
-            continue
-        if ingredients.count(M - ingredients[i]) != 0:
-            index = ingredients.index(M - ingredients[i])
-            if i == index:
-                continue
-            ingredients[index] = 0
-            ingredients[i] = 0
+    left, right = 0, N -1
+    while left < right:
+        temp = ingredients[left] + ingredients[right]
+        if temp == M:
             answer += 1
+            left += 1
+            right -= 1
+        elif temp < M:
+            left += 1
+        else:
+            right -= 1
     print(answer)
 
-
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
